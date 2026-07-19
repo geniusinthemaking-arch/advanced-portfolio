c5a14527cdfe3ddf50bf3079faa8ffbe667bb845
Alyssah Van Orman <285124057+geniusinthemaking-arch@users.noreply.github.com>
2026-07-19 17:53:32 -0600

    Fixed dark-theme

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let contrastToggle = false
 function toggleContrast() {
     contrastToggle = !contrastToggle
     if (contrastToggle) {
-    document.body.classList += " dark-theme"
+        document.body.classList += " dark-theme"
     }
     else {
         document.body.classList.remove("dark-theme")
